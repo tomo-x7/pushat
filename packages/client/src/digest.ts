657d040f66cb414dc873eca7c31e7677916f8ff8
celastrina.argiolis.tk <celastrina.argiolis.tk@gmail.com>
2025-10-03 03:31:01 +0000

    update

--- packages/client/src/digest.ts
+++ packages/client/src/digest.ts
@@ -11,28 +11,25 @@ async function generateContentDigest(input: ArrayBufferLike | Buffer) {
 }
 /**
  * SHA-512 only
- * copy request and add Content-Digest header
+ * @returns value of Content-Digest header
  */
-export async function addContentDigest(req:Request):Promise<Request>{
-	
-	const data=(await req.clone().arrayBuffer())
-	const digest=await generateContentDigest(data)
-	const headers=new Headers(req.headers)
-	headers.set("Content-Digest",`sha-512=:${digest}:`)
-	const newReq=new Request(req,{headers})
-	return newReq;
+export async function getContentDigest(req: Request): Promise<string> {
+	const data = await req.clone().arrayBuffer();
+	const digest = await generateContentDigest(data);
+	return `sha-512=:${digest}:`;
 }
 /**
  * SHA-512 only
  * validate content digest header
  */
-export async function validateContentDigest(req:Request):Promise<true|string>{
-	const headerValue=req.headers.get("Content-Digest")??req.headers.get("content-digest")
-	if(headerValue==null)return "Content-Digest header required";
-	const match=headerValue.match(/^sha-512=:([a-zA-Z+\/=]+):$/)
-	if(match==null||match[1]==null)return "invalid Content-Digest header: multiple digest is not supported and sha-512 only."
-	const digest1=match[1];
-	const digest2=await generateContentDigest(await req.clone().arrayBuffer());
-	if(digest1!==digest2)return "invalid Content-Digest header: digest not match";
+export async function validateContentDigest(req: Request): Promise<true | string> {
+	const headerValue = req.headers.get("Content-Digest") ?? req.headers.get("content-digest");
+	if (headerValue == null) return "Content-Digest header required";
+	const match = headerValue.match(/^sha-512=:([a-zA-Z+/=]+):$/);
+	if (match == null || match[1] == null)
+		return "invalid Content-Digest header: multiple digest is not supported and sha-512 only.";
+	const digest1 = match[1];
+	const digest2 = await generateContentDigest(await req.clone().arrayBuffer());
+	if (digest1 !== digest2) return "invalid Content-Digest header: digest not match";
 	return true;
 }
--- packages/client/src/signature.ts
+++ packages/client/src/signature.ts
@@ -1,26 +1,46 @@
 import { Buffer } from "buffer";
 import { toBuffer } from "./util.js";
 
-type a = EcdsaParams;
 
-export async function importJwk(jwkStr:string):Promise<CryptoKey> {
+export type CryptoKeyWithKid={
+	key:CryptoKey
+	kid:string
+}
+/**
+ * ECDSA only
+ * @param jwkStr private JWK
+ * @returns CryptoKey
+ */
+export async function importJwk(jwkStr: string): Promise<CryptoKeyWithKid> {
 	const jwk = JSON.parse(jwkStr);
-	const {kty,crv,alg,d,x,y}=jwk as JsonWebKey
-	if(kty!=="EC") throw new Error("kty must be EC");
-	if(x==null||y==null) throw new Error("invalid key");
-	if(!crv) throw new Error("crv is required");
-	if(d==null) throw new Error("private key required");
-	return crypto.subtle.importKey("jwk",jwk,{name:"ECDSA",namedCurve:crv},false,["sign"]);
+	const { kty, crv, alg, d, x, y,kid } = jwk as JsonWebKey&{kid:string};
+	if (kty !== "EC") throw new Error("kty must be EC");
+	if (x == null || y == null) throw new Error("invalid key");
+	if (!crv) throw new Error("crv is required");
+	if (d == null) throw new Error("private key required");
+	if(kid==null)throw new Error("kid required")
+	if(/^did:[a-z0-9]+:[a-zA-Z0-9.]+#[a-zA-Z0-9]+$/.test(kid))throw new Error("invalid kid: kid must be did with fragment")
+	if(!kid.startsWith("did:web:")||!kid.startsWith("did:plc:"))throw new Error("unspported did method")
+	return {key:await crypto.subtle.importKey("jwk", jwk, { name: "ECDSA", namedCurve: crv }, false, ["sign"]),kid};
 }
 
-/**ECDSA only */
-export async function signRequest(req: Request, key: CryptoKey) {
+/**
+ * ECDSA only
+ * @returns {{"Signature-Input":string, "Signature":string}}
+ */
+export async function signRequest(
+	req: Request,
+	{key,kid}: CryptoKeyWithKid
+): Promise<{ "Signature-Input": string; Signature: string }> {
+	const sigInput=`pushat=("@target-uri" "content-digest");created=${new Date().valueOf()};keyid=${kid}`
 	const data = await generateData(req);
-	await crypto.subtle.sign({ name: "ECDSA", hash: { name: "SHA-512" } }, key, data);
+	const signed=await crypto.subtle.sign({ name: "ECDSA", hash: { name: "SHA-512" } }, key, data);
+	const Signature=Buffer.from(signed).toString("base64")
+	return { "Signature-Input":sigInput , Signature };
 }
 
 async function generateData(req: Request, urlBase?: string): Promise<Buffer> {
 	const bodyBuf = toBuffer(await req.clone().arrayBuffer());
-	const urlBuf = Buffer.from(new URL(req.url, urlBase).toString());
-	return Buffer.concat([bodyBuf, urlBuf]);
+	const url = (new URL(req.url, urlBase).toString());
+	return Buffer.from()
 }
